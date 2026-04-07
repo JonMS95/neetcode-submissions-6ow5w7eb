431a0c390507dce18a114e137e6db8b944e951aa
Jon <107001163+JonMS95@users.noreply.github.com>
2026-04-07 20:16:20 +0200

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.cpp
@@ -0,0 +1,49 @@
+class Solution {
+private:
+    std::queue<int> _enqueueFigures(int x)
+    {
+        std::queue<int> q;
+
+        while(x > 0)
+        {
+            q.push(x % 10);
+            x /= 10;
+        }
+
+        return q;
+    }
+
+public:
+    int reverse(int x)
+    {
+        bool is_negative = x < 0 ? true : false;
+
+        if(is_negative)
+            x = -x;
+
+        std::queue<int> q = this->_enqueueFigures(x);
+
+        int ret = 0;
+
+        while(!q.empty())
+        {
+            if(ret > (INT_MAX / 10))
+                return 0;
+            
+            ret = ret * 10 + q.front();
+
+            // if(is_negative && ret > 0)
+            // {
+            //     is_negative = false;
+            //     ret -= ret;
+            // }
+
+            q.pop();
+        }
+
+        if(is_negative)
+            ret = -ret;
+
+        return ret;
+    }
+};
